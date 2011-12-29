aa7a6baab2bb12aaf1369f90e92c288caf2bab69
Manuel Gomez <manuel.gomez@kodesoft.net>
2011-12-29 20:02:12 +0100

    repeated words stats jquery functionality

---
+++ analyzr/Content/js/analyzr.js
@@ -0,0 +1,43 @@
+﻿jQuery(document).ready(function ($) {
+    $("#myform").submit(function (event) {
+        dump_text($("#inputText"), $("#output_text"));
+        build_word_list($("#inputText"), $("#word_list"));
+        $("#repetitions_container").show();
+        return false;
+    });
+
+    function dump_text(from, to) {
+        var text = from.val().trim();
+        text = text.replace(/\n/g, "</p><p>");
+        to.html("<p>" + text + "</p>");
+    }
+
+    function build_word_list(from, item) {
+        var wordlist = list_repeated_words(from.val());
+        var html_text = "";
+        for (var i = 0; i < wordlist.length; i++) {
+            html_text += " <span class=\"word\">" + wordlist[i] + "</span>";
+        }
+        item.html(html_text);
+
+        $("span.word").click(function (event) {
+            var text = $(this).text();
+            $("#output_text").removeHighlight();
+            $("#output_text").highlight(text);
+
+        });
+    }
+
+    function list_repeated_words(text) {
+        var sortedWords = text.split(/\b(\w+)\b/g);
+        sortedWords.sort();
+        duplicateWords = [];
+        for (var i = 0; i < sortedWords.length - 1; i++) {
+            if (sortedWords[i + 1] == sortedWords[i]) {
+                duplicateWords.push(sortedWords[i]);
+            }
+        }
+        return $.unique(duplicateWords.sort());
+    }
+
+});
